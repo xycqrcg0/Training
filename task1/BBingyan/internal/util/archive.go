259de22509e85736ef30d4acf571a9acba60be87
xy <18726309979@163.com>
2025-04-17 17:07:06 +0800

    feat: sync data function

--- task1/BBingyan/internal/util/archive.go
+++ task1/BBingyan/internal/util/archive.go
@@ -1,1 +1,154 @@
 package util
+
+import (
+	"BBingyan/internal/global"
+	"BBingyan/internal/log"
+	"BBingyan/internal/model"
+	"github.com/go-redis/redis"
+	"gorm.io/gorm"
+	"strconv"
+	"strings"
+)
+
+var (
+	LIKE       = "1"
+	NEEDUNLIKE = "-1"
+)
+
+func Archive() error {
+	//把redis里的点赞信息往postgres里写
+
+	//user
+	userLikeKeys, err := global.RedisDB.Keys("userlike:*").Result() //会阻塞？
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	userLikeValues, err := global.RedisDB.MGet(userLikeKeys...).Result()
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	l1 := len(userLikeKeys)
+
+	userLikesKeys, err := global.RedisDB.Keys("userlikes:*").Result() //会阻塞？
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	userLikesValues, err := global.RedisDB.MGet(userLikeKeys...).Result()
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	l2 := len(userLikeKeys)
+
+	err = global.DB.Transaction(func(tx *gorm.DB) error {
+		for i := 0; i < l1; i++ {
+			if userLikeValues[i] == LIKE {
+				params := strings.Split(userLikeKeys[i], ":")
+				tx.Model(&model.UserLikeShip{}).Create(&model.UserLikeShip{
+					User:      params[1],
+					LikedUser: params[2],
+				})
+			} else if userLikeValues[i] == NEEDUNLIKE {
+				params := strings.Split(userLikeKeys[i], ":")
+				tx.Model(&model.UserLikeShip{}).Where("user=? AND liked_user=?", params[1], params[2]).Delete(&model.UserLikeShip{})
+			}
+		}
+
+		for i := 0; i < l2; i++ {
+			params := strings.Split(userLikesKeys[i], ":")
+			tx.Model(&model.User{}).Where("email=?", params[1]).Update("likes", userLikesValues[i])
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Errorf("Fail to finish postgres transaction,error:%v", err)
+		return err
+	}
+	//redis里记录删了
+	_, e := global.RedisDB.TxPipelined(func(pipe redis.Pipeliner) error {
+		for _, key := range userLikeKeys {
+			pipe.Del(key)
+		}
+		for _, key := range userLikesKeys {
+			pipe.Del(key)
+		}
+		return nil
+	})
+	if e != nil {
+		log.Errorf("Fail to write redis,error:%v", err)
+		return err
+	}
+
+	//post
+	postLikeKeys, err := global.RedisDB.Keys("postlike:*").Result() //会阻塞？
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	postLikeValues, err := global.RedisDB.MGet(postLikeKeys...).Result()
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	l1 = len(postLikeKeys)
+
+	postLikesKeys, err := global.RedisDB.Keys("postlikes:*").Result() //会阻塞？
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	postLikesValues, err := global.RedisDB.MGet(postLikeKeys...).Result()
+	if err != nil {
+		log.Errorf("Fail to read redis,error:%v", err)
+		return err
+	}
+	l2 = len(postLikeKeys)
+
+	err = global.DB.Transaction(func(tx *gorm.DB) error {
+		for i := 0; i < l1; i++ {
+			if postLikeValues[i] == LIKE {
+				params := strings.Split(postLikeKeys[i], ":")
+				post, _ := strconv.Atoi(params[2])
+				tx.Model(&model.PostLikeShip{}).Create(&model.PostLikeShip{
+					User:      params[1],
+					LikedPost: post,
+				})
+			} else if postLikeValues[i] == NEEDUNLIKE {
+				params := strings.Split(postLikeKeys[i], ":")
+				tx.Model(&model.PostLikeShip{}).Where("user=? AND liked_post=?", params[1], params[2]).Delete(&model.PostLikeShip{})
+			}
+		}
+
+		for i := 0; i < l2; i++ {
+			params := strings.Split(postLikesKeys[i], ":")
+			tx.Model(&model.Post{}).Where("id=?", params[1]).Update("likes", postLikesValues[i])
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Errorf("Fail to finish postgres transaction,error:%v", err)
+		return err
+	}
+	//redis里记录删了
+	_, e = global.RedisDB.TxPipelined(func(pipe redis.Pipeliner) error {
+
+		for _, key := range postLikeKeys {
+			global.RedisDB.Del(key)
+		}
+		for _, key := range postLikesKeys {
+			global.RedisDB.Del(key)
+		}
+		return nil
+	})
+	if e != nil {
+		log.Errorf("Fail to write redis,error:%v", err)
+		return err
+	}
+
+	return nil
+}
